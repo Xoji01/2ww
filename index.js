e656bbec2c79c5f607b649e628a4a36d407dbbbe
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-09-28 15:15:02 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let nums = 0
 let others= 0
 let str = 0
 
-let qq = arr.filter(item =>typeof(item)==='string').length
+let qq = arr.filter(item =>typeof item==='string').length
 console.log(qq)
-let ww = arr.filter (num =>typeof(num) === 'number').length
+let ww = arr.filter (num =>typeof num === 'number').length
 console.log(ww);
-let ee = arr.filter (other => typeof(other) !==isNaN).length
+let ee = arr.filter (other => typeof other !==isNaN).length
 console.log(ee);
